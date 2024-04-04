894fa6199478afc6662875eea672c4f5a26b9a61
tom <lim1017@hotmail.com>
2024-04-04 01:40:41 -0400

    get review route

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,5 +1,20 @@
+import BarStoolPizza from "../data/BarStoolPizza.json";
+import { NextResponse } from "next/server";
+
 export const GET = async (req, res) => {
   const searchParams = req.nextUrl.searchParams;
 
   const channelName = searchParams.get("channelName");
+
+  let data;
+  switch (channelName) {
+    case "BarStoolPizza":
+      data = BarStoolPizza;
+      break;
+    default:
+      res.status(404).json({ error: "Channel not found" });
+      return;
+  }
+
+  return NextResponse.json({ data }, { status: 200 });
 };
